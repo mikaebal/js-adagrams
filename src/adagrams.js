154652be02e159a4515e422c6cb60746e1fb8f0c
Mikaela Baluyot <mikaebal@gmail.com>
2025-05-29 14:57:30 -0700

    feat: implement wave 3 scoreWord

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,30 +1,18 @@
 const LETTER_POOL = {
-  A: 9,
-  B: 2,
-  C: 2,
-  D: 4,
-  E: 12,
-  F: 2,
-  G: 3,
-  H: 2,
-  I: 9,
-  J: 1,
-  K: 1,
-  L: 4,
-  M: 2,
-  N: 6,
-  O: 8,
-  P: 2,
-  Q: 1,
-  R: 6,
-  S: 4,
-  T: 6,
-  U: 4,
-  V: 2,
-  W: 2,
-  X: 1,
-  Y: 2,
-  Z: 1,
+  A: 9, B: 2, C: 2, D: 4, E: 12, F: 2, G: 3, H: 2,
+  I: 9, J: 1, K: 1, L: 4, M: 2, N: 6, O: 8, P: 2,
+  Q: 1, R: 6, S: 4, T: 6, U: 4, V: 2, W: 2, X: 1,
+  Y: 2, Z: 1,
+};
+
+const SCORE_CHART = {
+  A: 1, E: 1, I: 1, O: 1, U: 1, L: 1, N: 1, R: 1, S: 1, T: 1,
+  D: 2, G: 2,
+  B: 3, C: 3, M: 3, P: 3,
+  F: 4, H: 4, V: 4, W: 4, Y: 4,
+  K: 5,
+  J: 8, X: 8,
+  Q: 10, Z: 10,
 };
 
 // wave 1
@@ -47,7 +35,7 @@ export const drawLetters = () => {
     const randIndex = Math.floor(Math.random() * letterArray.length);
     const letter = letterArray[randIndex];
 
-    const letterCountInHand = hand.filter(l => l === letter).length;
+    const letterCountInHand = hand.filter(letterInHand => letterInHand === letter).length;
 
     if (letterCountInHand < LETTER_POOL[letter]) {
       hand.push(letter);
@@ -72,7 +60,20 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 
 // wave 3
 export const scoreWord = (word) => {
-  // Implement this method for wave 3
+  if (!word) return 0;
+
+  const upperWord = word.toUpperCase();
+  let score = 0;
+
+  for (let char of upperWord) {
+    score += SCORE_CHART[char] || 0;
+  }
+
+  if (upperWord.length >= 7) {
+    score += 8;
+  }
+
+  return score;
 };
 
 // wave 4
